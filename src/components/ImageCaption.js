db240c85afa9eb1be0c6ce19d51e3764c48f7dcd
Marcelle <yanarimy@gmail.com>
2019-06-04 13:03:45 -0300

    counter in image caption

--- src/components/ImageCaption.js
+++ src/components/ImageCaption.js
@@ -4,10 +4,14 @@ import React from 'react';
 import cn from 'classnames';
 
 function ImageCaption ({className, image, images}) {
+  const currentImageIndex = images.indexOf(image) + 1;
+  // TODO mudar o counter de acordo com o idioma
   return (
     <div className = {cn(className, 'caption')}>
       <figcaption>{image.caption}</figcaption>
-      <div className = "counter">{images.indexOf(image) + 1} de {images.length}</div>
+      <div className = "counter">
+        {currentImageIndex} de {images.length}
+      </div>
     </div>
   );
 }
