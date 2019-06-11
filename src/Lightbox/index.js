ed15fdb45080e9fee490802e8a64d54fce4338d7
Marcelle <yanarimy@gmail.com>
2019-06-11 17:04:19 -0300

    hooks to class, margin fixed

--- src/Lightbox/index.js
+++ src/Lightbox/index.js
@@ -1,51 +1,57 @@
 import './index.css';
 
-import React, {useEffect} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import MountTransition from 'mount-transition';
 import Portal from '../components/Portal';
 import CloseButton from "../components/CloseButton";
 import Media from "../components/Media";
 
-function Lightbox (props) {
-  const {
-    isOpen,
-    mediaList,
-    onClose,
-  } = props;
-  useEffect(() => {
+class Lightbox extends Component {
+  componentDidMount () {
+    const {isOpen} = this.props;
     if (isOpen) document.body.style.overflow = 'hidden';
     else document.body.style.overflow = 'auto';
-  }, [isOpen]);
-  return (
-    <MountTransition
-      className = "modal"
-      isMounted = {isOpen}
-      preset = "fade"
-    >
-      <div className = "container">
-        {mediaList.map((data) => {
-          return (
-            <Media currentMedia = {data}/>
-          );
-        })}
-      </div>
-      <div className = "close__button">
-        <CloseButton onClick = {onClose}/>
-      </div>
-    </MountTransition>
-  );
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.isOpen !== prevProps.isOpen) {
+      if (this.props.isOpen) document.body.style.overflow = 'hidden';
+      else document.body.style.overflow = 'auto';
+    }
+  }
+
+  render () {
+    const {
+      isOpen,
+      mediaList,
+      onClose,
+    } = this.props;
+    return (
+      <MountTransition
+        className = "modal"
+        isMounted = {isOpen}
+        preset = "fade"
+      >
+        <div className = "container">
+          {mediaList.map((data) => {
+            return (
+              <Media currentMedia = {data}/>
+            );
+          })}
+        </div>
+        <div className = "close__button">
+          <CloseButton onClick = {onClose}/>
+        </div>
+      </MountTransition>
+    );
+  }
 }
 
 Lightbox.propTypes = {
-  currentMediaIndex: PropTypes.number,
   isOpen: PropTypes.bool.isRequired,
   mediaList: PropTypes.array.isRequired,
   onClose: PropTypes.func.isRequired,
-};
-
-Lightbox.defaultProps = {
-  currentMediaIndex: 0,
 };
 
 export default Portal('portal')(Lightbox);
